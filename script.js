55deb0c971a98e981c5e99f5d18252f40b87b571
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2023-08-23 09:27:44 +1000

    Add turn counter and player selector in console

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ selectDropDown.addEventListener("change", function () {
   player2.takeTurn();
 });
 
+let activePlayer = "";
+
 const gameBoardModule = (function () {
   const gameBoardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   return { gameBoardArray };
@@ -11,19 +13,27 @@ let gameBoard = gameBoardModule.gameBoardArray;
 // console.log(gameBoard);
 
 function playerFactory(name, playerMarker) {
-  let turnCounter = 0;
   return {
+    turnCounter: 0,
     name,
     takeTurn() {
-      turnCounter++;
+      this.turnCounter++;
       gameBoard.splice(selectDropDown.value, 1, playerMarker);
-      console.log(turnCounter);
-      console.log(gameBoard);
+      //   console.log(gameBoard);
+      console.log(this.turnCounter);
+      return this.turnCounter;
     },
   };
 }
 
 player1 = playerFactory("Jack", "X");
 player2 = playerFactory("Beans", "O");
-// player1.takeTurn();
-// player2.takeTurn();
+console.log(player1.turnCounter);
+
+console.log(player1.turnCounter);
+
+const gameFlowModule = (function () {
+  if (player1.turnCounter > player2.turnCounter) {
+    console.log("player 2's go");
+  } else console.log("player 1's go");
+})();
