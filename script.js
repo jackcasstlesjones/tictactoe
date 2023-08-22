b87f78bbe83202cc3cfed887dc64b8d73ba875d4
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2023-08-23 09:07:47 +1000

    Add player markers X and O

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-function playerFactory(name) {
-  let turnCounter = 0;
-  return {
-    name,
-    takeTurn() {
-      turnCounter++;
-      console.log(turnCounter);
-    },
-  };
-}
+const selectDropDown = document.getElementById("number");
+selectDropDown.addEventListener("change", function () {
+  player2.takeTurn();
+});
 
 const gameBoardModule = (function () {
   const gameBoardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   return { gameBoardArray };
 })();
+let gameBoard = gameBoardModule.gameBoardArray;
+// console.log(gameBoard);
 
-console.log(gameBoardModule.gameBoardArray);
+function playerFactory(name, playerMarker) {
+  let turnCounter = 0;
+  return {
+    name,
+    takeTurn() {
+      turnCounter++;
+      gameBoard.splice(selectDropDown.value, 1, playerMarker);
+      console.log(turnCounter);
+      console.log(gameBoard);
+    },
+  };
+}
 
-player1 = playerFactory("Jack");
-player1.takeTurn();
+player1 = playerFactory("Jack", "X");
+player2 = playerFactory("Beans", "O");
+// player1.takeTurn();
+// player2.takeTurn();
